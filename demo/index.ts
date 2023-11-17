13a2099310b89f6565a1e7316921cfe3c0974e71
Tom MacWright <tom@macwright.com>
2023-11-17 13:56:21 -0500

    Add vite workflow

--- demo/index.ts
+++ demo/index.ts
@@ -15,41 +15,43 @@ import {
   tsSync,
 } from "../src/index.js";
 
-const fsMap = await createDefaultMapFromCDN(
-  { target: ts.ScriptTarget.ES2022 },
-  "3.7.3",
-  true,
-  ts,
-);
-const system = createSystem(fsMap);
-const compilerOpts = {};
-const env = createVirtualTypeScriptEnvironment(system, [], ts, compilerOpts);
+(async () => {
+  const fsMap = await createDefaultMapFromCDN(
+    { target: ts.ScriptTarget.ES2022 },
+    "3.7.3",
+    true,
+    ts,
+  );
+  const system = createSystem(fsMap);
+  const compilerOpts = {};
+  const env = createVirtualTypeScriptEnvironment(system, [], ts, compilerOpts);
 
-const path = "index.ts";
+  const path = "index.ts";
 
-let editor = new EditorView({
-  doc: `let hasAnError: string = 10;
+  let editor = new EditorView({
+    doc: `let hasAnError: string = 10;
 
 function increment(num: number) {
   return num + 1;
 }
 
 increment('not a number');`,
-  extensions: [
-    basicSetup,
-    javascript({
-      typescript: true,
-      jsx: true,
-    }),
-    tsSync({ env, path }),
-    tsLinter({ env, path }),
-    autocompletion({
-      override: [tsAutocomplete({ env, path })],
-    }),
-    tsHover({
-      env,
-      path,
-    }),
-  ],
-  parent: document.querySelector("#editor")!,
-});
+    extensions: [
+      basicSetup,
+      javascript({
+        typescript: true,
+        jsx: true,
+      }),
+      tsSync({ env, path }),
+      tsLinter({ env, path }),
+      autocompletion({
+        override: [tsAutocomplete({ env, path })],
+      }),
+      tsHover({
+        env,
+        path,
+      }),
+    ],
+    parent: document.querySelector("#editor")!,
+  });
+})();
