f91a46e7dd8c4bba7591d2c713466ce795a21e9f
Tom MacWright <tmcw@users.noreply.github.com>
2025-02-07 13:26:51 -0500

    Provide ATA example (#57)

--- demo/index.ts
+++ demo/index.ts
@@ -1,23 +1,23 @@
-import { EditorView, basicSetup } from "codemirror";
-import { javascript } from "@codemirror/lang-javascript";
 import { autocompletion } from "@codemirror/autocomplete";
+import { javascript } from "@codemirror/lang-javascript";
 import {
   createDefaultMapFromCDN,
   createSystem,
   createVirtualTypeScriptEnvironment,
 } from "@typescript/vfs";
-import ts from "typescript";
+import { EditorView, basicSetup } from "codemirror";
+import * as Comlink from "comlink";
+import type ts from "typescript";
 import {
-  tsLinter,
-  tsHover,
   tsAutocomplete,
-  tsSync,
   tsFacet,
   tsGoto,
+  tsHover,
+  tsLinter,
+  tsSync,
   tsTwoslash,
 } from "../src/index.js";
-import * as Comlink from "comlink";
-import { WorkerShape } from "../src/worker.js";
+import type { WorkerShape } from "../src/worker.js";
 
 function renderDisplayParts(dp: ts.SymbolDisplayPart[]) {
   const div = document.createElement("div");
@@ -39,7 +39,7 @@ function renderDisplayParts(dp: ts.SymbolDisplayPart[]) {
   const worker = Comlink.wrap(innerWorker) as WorkerShape;
   await worker.initialize();
 
-  let editor = new EditorView({
+  const editor = new EditorView({
     doc: `let hasAnError: string = 10;
 
 function increment(num: number) {
@@ -86,5 +86,60 @@ increment('not a number');`,
       tsTwoslash(),
     ],
     parent: document.querySelector("#editor-worker")!,
+  });
+})().catch((e) => console.error(e));
+
+(async () => {
+  const path = "index.ts";
+
+  // TODO: this is the one place where we can't use .js urls
+  const innerWorker = new Worker(new URL("./worker_ata.ts", import.meta.url), {
+    type: "module",
+  });
+  const worker = Comlink.wrap(innerWorker) as WorkerShape;
+  await worker.initialize();
+
+  const editor = new EditorView({
+    doc: `import { min } from "simple-statistics";
+
+const minimumValue = min([1, 2, 3]);`,
+    extensions: [
+      basicSetup,
+      javascript({
+        typescript: true,
+        jsx: true,
+      }),
+      tsFacet.of({ worker, path }),
+      tsSync(),
+      tsLinter(),
+      autocompletion({
+        override: [
+          tsAutocomplete({
+            renderAutocomplete(raw) {
+              return () => {
+                const div = document.createElement("div");
+                if (raw.documentation) {
+                  const description = div.appendChild(
+                    document.createElement("div"),
+                  );
+                  description.appendChild(
+                    renderDisplayParts(raw.documentation),
+                  );
+                }
+                if (raw?.displayParts) {
+                  const dp = div.appendChild(document.createElement("div"));
+                  dp.appendChild(renderDisplayParts(raw.displayParts));
+                }
+                return { dom: div };
+              };
+            },
+          }),
+        ],
+      }),
+      tsHover(),
+      tsGoto(),
+      tsTwoslash(),
+    ],
+    parent: document.querySelector("#editor-worker-ata")!,
   });
 })().catch((e) => console.error(e));
